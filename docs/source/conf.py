2150649a4843f94c4078efe06dcb558505a5a59f
_ <4256466+leehart@users.noreply.github.com>
2023-10-05 15:35:48 +0100

    Add malariagen_data package, poetry.lock, pyproject.toml, docs source, docs config, docs workflows, .gitignore

---
+++ docs/source/conf.py
@@ -0,0 +1,51 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+import os
+import sys
+sys.path.insert(0, os.path.abspath('../..'))
+import malariagen_data
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'malariagen_data API'
+copyright = '2023, MalariaGEN'
+author = 'MalariaGEN'
+release = malariagen_data.__version__
+version = 'v' + str(malariagen_data.__version__)
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['sphinx.ext.autosummary']
+
+autosummary_generate = True
+
+templates_path = ['_templates']
+exclude_patterns = []
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'pydata_sphinx_theme'
+html_theme_options = {
+    "switcher": {
+        "check_switcher": True,
+        "version_match": version,
+        "json_url": f"https://leehart.github.io/tmp_test_7/v0.0.0/_static/switcher.json"
+    },
+    "navbar_center": ["version-switcher", "navbar-nav"],
+    "icon_links": [
+        {
+            "name": "GitHub",
+            "url": "https://github.com/malariagen/malariagen-data-python",
+            "icon": "fa-brands fa-github",
+        }
+    ],
+}
+html_static_path = ['_static']
+html_logo = "_static/logo.svg"
+html_favicon = "_static/favicon.ico"
